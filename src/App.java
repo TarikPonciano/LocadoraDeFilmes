b78d3a8744f7f8acca4d09b5769d92153dcb0bbb
Pedro Davi <pedrodavisousa@gmail.com>
2024-09-03 20:33:21 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -16,12 +16,28 @@ public class App {
             System.out.println("4. Ver Filmes de Terror");
             System.out.println("5. Ver Filmes de Ficção Científica");
             System.out.println("6. Ver Filmes Nacionais");
-
+            System.out.println("");
+            System.out.println("Digite a opção:");
             int op = leitor.nextInt();
 
             //Substituir o println pela exibição na tela uma lista de 10 filmes daquele gênero.
             if (op == 1){// Pedro
                 System.out.println("Vendo Filmes de Ação");
+                String[] filmesAcao = new String[10];
+                filmesAcao[0] = "O Poderoso Chefão";
+                filmesAcao[1] = "Homem de Aço"; 
+                filmesAcao[2] = "O Último Grande Herói";
+                filmesAcao[3] = "Tropa de Elite";
+                filmesAcao[4] = "Deixe estar";
+                filmesAcao[5] = "Missão Impossível";
+                filmesAcao[6] = "O Justiceiro";
+                filmesAcao[7] = "Bater ou Correr";
+                filmesAcao[8] = "O Fugitivo";
+                filmesAcao[9] = "O Grande Golpe";
+                for (int i = 0; i < 10; i++) {
+                    System.out.println((i+1) + ". " + filmesAcao[i]);
+                    } 
+                
             }else if (op == 2){// Ryan
                 System.out.println("Vendo Filmes de Comédia");
             }else if (op == 3){// Pietra e André
